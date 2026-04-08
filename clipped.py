c26e5e0be08ba6a1386e707f21fcb10f42d225ac
David <davvan@pdx.edu>
2026-04-08 13:26:17 -0700

    created clipped wave

--- clipped.py
+++ clipped.py
@@ -13,11 +13,7 @@ array = np.linspace(
     0.0, DURATION, SAMPLE_RATE
 )
 
-pure_wave = 8192 * np.sin(2 * np.pi * FREQUENCY * array)
-pure_wave_16bit = pure_wave.astype(np.int16)
-print(pure_wave_16bit)
-
-def make_wave(frequency, duration, amplitude, sample_rate=48000):
+def make_wave(amplitude, frequency=440, duration=1, sample_rate=48000):
     total_samples = sample_rate*duration
     array = np.linspace(
         0.0, duration, total_samples
@@ -29,6 +25,13 @@ def make_wave(frequency, duration, amplitude, sample_rate=48000):
 
     return pure_wave_16bit
     
-sin_wave = make_wave(440,1,8192)
-
+sin_wave = make_wave(8192)
 wavfile.write("sine.wav", SAMPLE_RATE, sin_wave)
+
+#make a bigger wave but we will clip us numpy
+louder_wav = make_wave(16384)
+
+#clip it at +-8192
+clip_wave = np.clip(louder_wav,-8192,8192)
+wavfile.write("clipped.wav", SAMPLE_RATE, clip_wave)
+
