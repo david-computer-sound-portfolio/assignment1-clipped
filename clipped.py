2fc0c7ec71758f9e29882abcaf84a7a321607f69
David <davvan@pdx.edu>
2026-04-13 22:50:43 -0700

    added readme

--- clipped.py
+++ clipped.py
@@ -1,17 +1,7 @@
 import numpy as np
 from scipy.io import wavfile
+import sounddevice as sd
 
-
-SAMPLE_RATE = 48000
-DURATION = 1
-TOTAL_SAMPLES = SAMPLE_RATE * DURATION
-FREQUENCY = 440
-AMPLITUDE = 8193
-
-
-array = np.linspace(
-    0.0, DURATION, SAMPLE_RATE
-)
 
 def make_wave(amplitude, frequency=440, duration=1, sample_rate=48000):
     total_samples = sample_rate*duration
@@ -26,12 +16,16 @@ def make_wave(amplitude, frequency=440, duration=1, sample_rate=48000):
     return pure_wave_16bit
     
 sin_wave = make_wave(8192)
-wavfile.write("sine.wav", SAMPLE_RATE, sin_wave)
+wavfile.write("sine.wav", sin_wave)
 
 #make a bigger wave but we will clip us numpy
 louder_wav = make_wave(16384)
 
 #clip it at +-8192
 clip_wave = np.clip(louder_wav,-8192,8192)
-wavfile.write("clipped.wav", SAMPLE_RATE, clip_wave)
+wavfile.write("clipped.wav", clip_wave)
 
+print("Playing clipped wave")
+sd.play(clip_wave)
+sd.wait()
+print("Finished Playing")
